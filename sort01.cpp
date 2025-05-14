3351df26730254464d6744be3af4c99fdfb4eb5d
Rohitadhav123 <adhavrohit37@gmail.com>
2025-05-15 04:45:28 +0530

    sorting of 0 and 1 problem add

---
+++ sort01.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+void sortone(int array[],int size)
+{
+    int left=0,right=0;
+    while(left<right)
+    {
+        for(int i=0;i<size;i++)
+        {
+            i++;
+        }
+    }
+}
+void display(int array[],int size)
+{
+    cout<<"Print array :"<<endl;
+    for(int i=0;i<size;i++)
+    {
+        cout<<array[i];
+    }
+}
+int main()
+{
+    int size;
+    cout<<"Enter the size of the array :"<<endl;
+    int array[size];
+    cout<<"Enter the array element :"<<endl;
+    for(int i=0;i<size;i++)
+    {
+        cin>>array[i];
+    }
+
+}

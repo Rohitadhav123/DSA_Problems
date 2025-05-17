ea29da569927d82584b678af166315db207175d8
Rohitadhav123 <adhavrohit37@gmail.com>
2025-05-17 10:12:25 +0530

    Add subpair.cpp

---
+++ subpair.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+void sub(int array[],int size,int key)
+{
+    for(int i=0;i<size;i++)
+    {
+        for(int j=0;j<size;j++)
+        {
+            
+        }
+    }
+}
+
+int main()
+{
+    int size;
+    cout<<"Enter the sizeof the array";
+    cin>>size;
+    int array[size];
+    cout<<"Element in the array :"<<endl;
+    for(int i=0;i<size;i++)
+    {
+        cin>>array[i];
+    }
+    int key;
+    cout<<"Entre the target value :";
+    cin>>key;
+}

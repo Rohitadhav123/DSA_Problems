f7513eabceaf3a72b3b52ba5fe0ed62ee5151ee6
Rohitadhav123 <159225705+Rohitadhav123@users.noreply.github.com>
2025-06-17 16:01:06 +0530

    Update Interview_binary_Search.cpp

--- Interview_binary_Search.cpp
+++ Interview_binary_Search.cpp
@@ -3,5 +3,14 @@ using namespace std;
 
 int main()
 {
+    int size;
+    cout<<"Enter Total number of array:";
+    cin>>size;
+    int array[size];
+    cout<<"Enter the element in the array:";
+    for(int i=0;i<size;i++)
+    {
     
+        cin>>array[i];
+    }
 }

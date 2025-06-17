8f7dedd642b353ad8f7e6dd4194f6084365c716f
Rohitadhav123 <159225705+Rohitadhav123@users.noreply.github.com>
2025-06-17 16:19:26 +0530

    FirstOrder_binary_Search.cpp

--- Interview_binary_Search.cpp
+++ Interview_binary_Search.cpp
@@ -1,6 +1,35 @@
 #include<iostream>
 using namespace std;
 
+int firstorder(int array[],int size,int key)
+{
+    int start=0;
+    int end=size-1;
+    int ans=0;
+    int mid=(start+end)/2;
+    while(start<=end)
+    {
+        if(array[mid]==key)
+        {
+            ans=mid;
+            start =mid+1;
+            return ans;
+        }
+        else if(array[mid]>key)
+        {
+            end=mid-1;
+        }
+        else if(array[mid]<key)
+        {
+            start=mid+1;
+        }
+    }
+    return -1;
+    
+    
+}
+
+
 int main()
 {
     int size;
@@ -13,4 +42,10 @@ int main()
     
         cin>>array[i];
     }
+    int key;
+    cout<<"Enter the search key:";
+    cin>>key;
+    
+    int answer=firstorder(array,size,key);
+    cout<<"The starting index is:"<<answer;
 }

9f209cbb5444743795aee35bea0083e842d2f2c5
Rohitadhav123 <adhavrohit37@gmail.com>
2025-06-28 10:29:10 +0530

    adding the selection sort

--- selection.cpp
+++ selection.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
 using namespace std;
+
+
 int main()
 {
+
+    int size;
+    cout<<"Enter the total size of array :";
+    cin>>size;
+    int array[size];
+    cout<<"Enter the element in array :";
+    for(int i=0;i<size;i++)
+    {
+        cin>>array[i];
+    }
+//     selection_sort(array,size);
     
-}
+//    sorted_display(array,size);
+// }

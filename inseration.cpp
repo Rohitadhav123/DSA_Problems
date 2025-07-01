681598d2dd8e1134524fea123c072b54a624bbd9
Rohitadhav123 <adhavrohit37@gmail.com>
2025-07-01 18:56:20 +0530

    add insetation sort

--- inseration.cpp
+++ inseration.cpp
@@ -3,5 +3,13 @@ using namespace std;
 
 int main()
 {
-    
+    int size;
+    cout<<"Enter the total element of array :"<<endl;
+    cin>>size;
+    int array[size];
+    cout<<"Enter the element in the array :"<<endl;
+    for(int i=0;i<size;i++)
+    {
+        cin>>array[i];
+    }
 }

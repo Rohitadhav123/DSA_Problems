785945ebd25d7bab4ce2d3f81963cc762411922e
Rohitadhav123 <adhavrohit37@gmail.com>
2025-07-03 16:23:19 +0530

    add inseration sort

--- inseration.cpp
+++ inseration.cpp
@@ -1,15 +1,45 @@
 #include<iostream>
 using namespace std;
 
+void insertion_sort(int array[], int size)
+{
+    for(int i = 1; i < size; i++)
+    {
+        int key = array[i];
+        int j = i - 1;
+
+        
+        while(j >= 0 && array[j] > key)
+        {
+            array[j + 1] = array[j];
+            j--;
+        }
+
+        array[j + 1] = key;  
+    }
+}
+
 int main()
 {
     int size;
-    cout<<"Enter the total element of array :"<<endl;
-    cin>>size;
+    cout << "Enter the total elements of array: ";
+    cin >> size;
+
     int array[size];
-    cout<<"Enter the element in the array :"<<endl;
-    for(int i=0;i<size;i++)
+    cout << "Enter the elements in the array: ";
+    for(int i = 0; i < size; i++)
     {
-        cin>>array[i];
+        cin >> array[i];
     }
+
+    insertion_sort(array, size);
+
+    cout << "Sorted array: ";
+    for(int i = 0; i < size; i++)
+    {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }

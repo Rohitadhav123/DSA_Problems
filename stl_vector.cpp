4cf33873df8a9e2c08ce383c373fb866c987d204
Rohitadhav123 <adhavrohit37@gmail.com>
2025-07-07 18:07:53 +0530

    add the vector

--- stl_vector.cpp
+++ stl_vector.cpp
@@ -10,6 +10,17 @@ int main()
     cout<<"Size-->"<<v.capacity()<<endl;
     v.push_back(2);
     cout<<"Size-->"<<v.capacity()<<endl;
-
-
+    cout<<"First element :"<<v.front()<<endl;
+    for(int i:v)
+    {
+        cout<<i<<" "<<endl;
+    }
+        // if size know of vector
+        vector<int> a(5,1);
+        // 5 is capacity means size
+        //1 is the value 5 time 1 is print
+        for(int i:a)
+        {
+            cout<<i<<endl;
+        }
 }

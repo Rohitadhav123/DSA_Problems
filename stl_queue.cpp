00888e4a2972a8ad622f06af51393ac97371d98a
Rohitadhav123 <adhavrohit37@gmail.com>
2025-07-08 09:20:26 +0530

    add the queue labrary

---
+++ stl_queue.cpp
@@ -0,0 +1,14 @@
+// FIFO
+
+#include<iostream>
+#include<queue>
+
+using namespace std;
+int main()
+{
+    queue<int> q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    cout<<"First element :"<<q.front()<<endl;
+}

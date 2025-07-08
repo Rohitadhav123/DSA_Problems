9d24341a9bae1d75e35aaac15fb6456b729bd68a
Rohitadhav123 <adhavrohit37@gmail.com>
2025-07-08 09:21:14 +0530

    add the stack labrary

---
+++ stl_stack.cpp
@@ -0,0 +1,15 @@
+
+//  LIFO
+
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main()
+{
+    stack<int> s;
+    s.push(1);
+    s.push(3);
+      cout<<"Stack top element :"<<s.top()<<endl;
+
+}

8dcb99491a6a88f9e712c718fd0e437bd17dd9a5
Rohitadhav123 <adhavrohit37@gmail.com>
2025-07-22 13:10:35 +0530

    add the additon of row in 2d array

--- addition_of_row_2darray.cpp
+++ addition_of_row_2darray.cpp
@@ -1,1 +1,56 @@
-x
+#include<iostream>
+using namespace std;
+
+void addition_of_row(int array[][3],int row,int col)
+{
+   
+    cout<<"Addition of row :"<<endl;
+    for(int row=0;row<3;row++)
+    {
+         int sum=0;
+        for(int col=0;col<3;col++)
+        {
+             
+            sum +=array[row][col];
+        }
+        cout<<sum<<endl;
+    }
+}
+void addition_of_col(int array[][3],int row,int col)
+{
+    for(int col=0;col<3;col++)
+    {
+        int sum=0;
+        for(int row=0;row<3;row++)
+        {
+            sum += array[row][col];
+        }
+        cout<<sum<<endl;
+    }
+}
+
+int main()
+{
+    int array[3][3];
+    cout<<"Enter the element in the array :"<<endl;
+    for(int row=0;row<3;row++)
+    {
+        for(int col=0;col<3;col++)
+        {
+            cin>>array[row][col];
+        }
+    }
+    cout<<"Display array :"<<endl;
+    for(int row=0;row<3;row++)
+    {
+        for(int col=0;col<3;col++)
+        {
+            cout<<array[row][col]<<" ";
+        }
+        cout<<endl;
+    }
+    addition_of_row(array,3,3);
+    cout<<"Addition of col :"<<endl;
+    addition_of_col(array,3,3);
+
+}

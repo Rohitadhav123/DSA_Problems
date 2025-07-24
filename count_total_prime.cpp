17f928c9b4f4ebec5c9bd5fe46a63f0e5d43c78d
Rohitadhav123 <adhavrohit37@gmail.com>
2025-07-24 18:07:09 +0530

    add count_total prime number

---
+++ count_total_prime.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void prime(int num)
+{
+    
+}
+
+int main()
+{
+    int num;
+    cout<<"Enter number for checking is prime or not :";
+    cin>>num;
+}

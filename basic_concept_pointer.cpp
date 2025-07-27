67b794f34c96d67f1ab2de3569d1c9a584e2a5cc
Rohitadhav123 <adhavrohit37@gmail.com>
2025-07-27 20:27:09 +0530

    add the basic concept of pointer

--- basic_concept_pointer.cpp
+++ basic_concept_pointer.cpp
@@ -4,9 +4,19 @@ using namespace std;
 int main()
 {
     int a=10;
-
+    double b=4.5;
+    double *ptr1=&b;
+    cout<<"double is :"<<b<<endl;
+    cout<<"double is ptr:"<<ptr1<<endl;
+    cout<<"double is ptr:"<<*ptr1++<<endl;
     int *ptr=&a;
     cout<<"value of a :"<<a<<endl;
     cout<<"value of ptr :"<<ptr<<endl;
+    (*ptr)++;
     cout<<"value of *ptr :"<<*ptr<<endl;
 }
+
+
+
+// * refrence operator
+// & show the address

939fe936c55d255d5a90c665df5a871df71bf1d0
Rohitadhav123 <adhavrohit37@gmail.com>
2025-08-05 12:58:06 +0530

    dubble pointer concept

--- dubble_pointer.cpp
+++ dubble_pointer.cpp
@@ -6,11 +6,17 @@ int main()
     int i=5;
     int *ptr=&i;
     int **ptr1=&ptr;
+  
+
+
     cout<<ptr<<endl;
     cout<<*ptr<<endl;
     cout<<ptr1<<endl;
     cout<<**ptr1<<endl;
+    cout<<&ptr1<<endl;
     cout<<*ptr1<<endl;
+    cout<<&i<<endl;
+    
 
 
 }
